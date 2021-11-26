bcf9a2df914a6e478dcf30faedd24eb0d5bd96d1
Aleksandar Jakimovski <a.jakimovski@admin-its-MacBook-Pro.local>
2021-11-26 10:07:48 +0100

    discrete keyboard movement

--- basics/basics.md
+++ basics/basics.md
@@ -264,3 +264,72 @@ while running:
 # End the game
 pygame.quit()
 ```
+---
+
+## 6. Discrete Keyboard Movement
+
+```python
+import pygame, os
+
+# Initialize pygame
+pygame.init()
+
+
+# Create display surface
+WIN_WIDTH = 600
+WIN_HEIGHT = 300
+WIN_RES = (WIN_WIDTH, WIN_HEIGHT)
+
+pygame.display.set_caption("Keyboard Movement")
+display_surface = pygame.display.set_mode(WIN_RES)
+
+# Define Colors
+
+BLACK = (0, 0, 0)
+
+# Setting up the path
+base_path = os.path.dirname(__file__)
+asset_path = os.path.join(base_path, "assets/")
+
+# Set game values
+VELOCITY = 20
+
+# Load in images
+dragon_image = pygame.image.load(asset_path + "dragon_right.png")
+dragon_rect = dragon_image.get_rect()
+dragon_rect.centerx = WIN_WIDTH // 2
+dragon_rect.bottom = WIN_HEIGHT
+
+# Game Loop
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        # Check for discrete movement
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                dragon_rect.x -= VELOCITY
+            if event.key == pygame.K_RIGHT:
+                dragon_rect.x += VELOCITY
+            if event.key == pygame.K_UP:
+                dragon_rect.y -= VELOCITY
+            if event.key == pygame.K_DOWN:
+                dragon_rect.y += VELOCITY
+
+    # Fill the display surface to cover old images
+    # If you don't do this it will leave the old images and it will look like Killua
+    display_surface.fill(BLACK)
+
+    # Blit assets to the screen
+    display_surface.blit(dragon_image, dragon_rect)
+
+    # Update display
+    pygame.display.update()
+
+
+# End Game
+pygame.quit()
+```
--- basics/basics.py
+++ basics/basics.py
@@ -1,49 +1,63 @@
 import pygame, os
 
-# Init pygame
+# Initialize pygame
 pygame.init()
+
+
+# Create display surface
+WIN_WIDTH = 600
+WIN_HEIGHT = 300
+WIN_RES = (WIN_WIDTH, WIN_HEIGHT)
+
+pygame.display.set_caption("Keyboard Movement")
+display_surface = pygame.display.set_mode(WIN_RES)
+
+# Define Colors
+
+BLACK = (0, 0, 0)
 
 # Setting up the path
 base_path = os.path.dirname(__file__)
 asset_path = os.path.join(base_path, "assets/")
 
-# Create a display surface
-WIN_WIDTH = 600
-WIN_HEIGHT = 300
-display_surface = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
-pygame.display.set_caption("Adding Sounds and Music")
+# Set game values
+VELOCITY = 20
 
-
-# Load sound effects
-sound_1 = pygame.mixer.Sound(asset_path + "sound_1.wav")
-sound_2 = pygame.mixer.Sound(asset_path + "sound_2.wav")
-
-# Play sound effects 
-sound_1.play()
-# If you don't delay the time (here 2 seconds) it will only play the last sound that is called
-pygame.time.delay(2000)
-
-# Change the volume of a sound effect
-sound_2.set_volume(.1)
-sound_2.play()
-
-# Load background music
-pygame.mixer.music.load(asset_path + "music.wav")
-
-# Play and stop the music
-# pygame.mixer.music.play() takes two arguments, first is how many tims you wanna loop, for example 1, 2, 3 etc (-1 for infinite loop), second argument is when do you want it to start
-pygame.mixer.music.play(-1) 
-pygame.time.delay(5000)
-pygame.mixer.music.stop()
+# Load in images
+dragon_image = pygame.image.load(asset_path + "dragon_right.png")
+dragon_rect = dragon_image.get_rect()
+dragon_rect.centerx = WIN_WIDTH // 2
+dragon_rect.bottom = WIN_HEIGHT
 
 # Game Loop
 running = True
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+        # Check for discrete movement
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                dragon_rect.x -= VELOCITY
+            if event.key == pygame.K_RIGHT:
+                dragon_rect.x += VELOCITY
+            if event.key == pygame.K_UP:
+                dragon_rect.y -= VELOCITY
+            if event.key == pygame.K_DOWN:
+                dragon_rect.y += VELOCITY
+
+    # Fill the display surface to cover old images
+    # If you don't do this it will leave the old images and it will look like Killua
+    display_surface.fill(BLACK)
+
+    # Blit assets to the screen
+    display_surface.blit(dragon_image, dragon_rect)
+
+    # Update display
     pygame.display.update()
 
-# End the game
+
+# End Game
 pygame.quit()
